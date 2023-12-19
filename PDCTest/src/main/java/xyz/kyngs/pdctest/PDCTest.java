340291ed8889ddf387065c44b28e59824fda7120
kyngs <kyngs@users.noreply.github.com>
2023-12-19 21:53:21 +0100

    Test entity, tile-entity and chunk PDC

--- PDCTest/src/main/java/xyz/kyngs/pdctest/PDCTest.java
+++ PDCTest/src/main/java/xyz/kyngs/pdctest/PDCTest.java
@@ -4,42 +4,80 @@ import com.infernalsuite.aswm.api.SlimePlugin;
 import com.infernalsuite.aswm.api.exceptions.*;
 import com.infernalsuite.aswm.api.world.properties.SlimePropertyMap;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.block.TileState;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
-public final class PDCTest extends JavaPlugin{
+public final class PDCTest extends JavaPlugin {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        var world = Bukkit.getWorld("pdctest");
+        if (sender instanceof Player player) {
+            var world = Bukkit.getWorld("pdctest");
+            var loc = new Location(world, 0, 0, 0);
 
-        switch (command.getName()) {
-            case "setpdc" -> {
-                world.getPersistentDataContainer().set(new NamespacedKey(this, "test"), PersistentDataType.STRING, "test");
-                sender.sendMessage("Set");
+            if (world.getBlockAt(loc).getType() != Material.CHEST) {
+                world.setBlockData(loc, Bukkit.createBlockData(Material.CHEST));
             }
-            case "getpdc" -> {
-                var pdc = world.getPersistentDataContainer().get(new NamespacedKey(this, "test"), PersistentDataType.STRING);
-                sender.sendMessage(pdc == null ? "null" : pdc);
-            }
-            case "removepdc" -> {
-                world.getPersistentDataContainer().remove(new NamespacedKey(this, "test"));
-                sender.sendMessage("Removed");
-            }
-            case "dumppdc" -> {
-                world.getPersistentDataContainer().getKeys().forEach(key -> sender.sendMessage(key.toString() + ":" + world.getPersistentDataContainer().get(key, PersistentDataType.STRING)));
+
+            var worldPdc = world.getPersistentDataContainer();
+            var chunkPdc = player.getLocation().getChunk().getPersistentDataContainer();
+            var playerPdc = player.getPersistentDataContainer();
+            var state = (TileState) world.getBlockAt(loc).getState();
+            var tileEntityPDC = state.getPersistentDataContainer();
+
+            switch (command.getName()) {
+                case "setpdc" -> {
+                    worldPdc.set(new NamespacedKey(this, "test"), PersistentDataType.STRING, "test");
+                    chunkPdc.set(new NamespacedKey(this, "test"), PersistentDataType.STRING, "test");
+                    playerPdc.set(new NamespacedKey(this, "test"), PersistentDataType.STRING, "test");
+                    tileEntityPDC.set(new NamespacedKey(this, "test"), PersistentDataType.STRING, "test");
+                    state.update();
+                    sender.sendMessage("Set");
+                }
+                case "getpdc" -> {
+                    var pd = worldPdc.get(new NamespacedKey(this, "test"), PersistentDataType.STRING);
+                    var chunkPd = chunkPdc.get(new NamespacedKey(this, "test"), PersistentDataType.STRING);
+                    var playerPd = playerPdc.get(new NamespacedKey(this, "test"), PersistentDataType.STRING);
+                    var tileEntityPd = tileEntityPDC.get(new NamespacedKey(this, "test"), PersistentDataType.STRING);
+                    sender.sendMessage(pd == null ? "null" : pd);
+                    sender.sendMessage(chunkPd == null ? "null" : chunkPd);
+                    sender.sendMessage(playerPd == null ? "null" : playerPd);
+                    sender.sendMessage(tileEntityPd == null ? "null" : tileEntityPd);
+                }
+                case "removepdc" -> {
+                    worldPdc.remove(new NamespacedKey(this, "test"));
+                    chunkPdc.remove(new NamespacedKey(this, "test"));
+                    playerPdc.remove(new NamespacedKey(this, "test"));
+                    tileEntityPDC.remove(new NamespacedKey(this, "test"));
+                    sender.sendMessage("Removed");
+                }
+                case "dumppdc" -> {
+                    worldPdc.getKeys().forEach(key -> sender.sendMessage(key.toString() + ":" + worldPdc.get(key, PersistentDataType.STRING)));
+                    sender.sendMessage("chunk");
+                    chunkPdc.getKeys().forEach(key -> sender.sendMessage(key.toString() + ":" + chunkPdc.get(key, PersistentDataType.STRING)));
+                    sender.sendMessage("player");
+                    playerPdc.getKeys().forEach(key -> sender.sendMessage(key.toString() + ":" + playerPdc.get(key, PersistentDataType.STRING)));
+                    sender.sendMessage("tileentity");
+                    tileEntityPDC.getKeys().forEach(key -> sender.sendMessage(key.toString() + ":" + tileEntityPDC.get(key, PersistentDataType.STRING)));
+                }
             }
         }
 
         return true;
-    };
+    }
+
+    ;
 
     @Override
     public void onEnable() {
